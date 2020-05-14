d04bbebd19cefb1194bdc128e7afd72653707fe2
Divakar Raut <divakar.raut@service.nsw.gov.au>
2020-05-14 20:27:49 +1000

    Practice Run

---
+++ MyFirstScript.js
@@ -0,0 +1,18 @@
+
+
+let myPet = "Dog";  // create a variable called myPet
+let petName =  "Rex"; // create a variable called petName
+let petAge = 6; //create a variable called petAge
+
+//let mySentence = `I have a ${mypet} called ${petName} who is ${petAge} years old`;
+
+console.log("I have a " + myPet + " " + "called" + " "+ petName + " "+ "who is "+ petAge + " "+"years old"); // concatenation method
+
+console.log(`I have a  ${myPet} calle ${petName}  who is ${petAge}  years old`);  // String Interpolation
+
+petAge= petAge + 2;
+
+console.log("I have a " + myPet + " " + "called" + " "+ petName + " "+ "who is "+ petAge + " "+"years old"); // concatenation method
+
+console.log(`I have a  ${myPet} calle ${petName}  who is ${petAge}  years old`);  // String Interpolation
+
